49c71e97decda19b6d82dcc4eb6200e95d675c53
natane <mimikaumi@gmail.com>
2020-12-19 15:11:17 +0900

    キャラクター

---
+++ Assets/Scripts/Character/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody EnemyRb;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private Vector3 enemyPos;
+
+    private float x;
+    private float y;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyPos = GetComponent<Rigidbody>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
