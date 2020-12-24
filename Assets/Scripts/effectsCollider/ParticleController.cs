1e319f100c4afd2acbf80d3917294afa0bc6ea21
natane010 <mimikaumi@hotmail.com>
2020-12-25 00:59:24 +0900

    エフェクト入れ途中　プレイヤー動きおｋ　未　ジャンプ　攻撃　標準のようなものをつける予定

---
+++ Assets/Scripts/effectsCollider/ParticleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        count++;
+        //パーティクルが他のコリジョンにヒットしたとき呼び出されるのでここにヒット系を入れたい
+    }
+}
