55085e9ac8d4266b14096a5fce7446c1edf617a8
natane <mimikaumi@gmail.com>
2021-02-08 16:02:23 +0900

    UI 以外直した　UIまだやばい(´;ω;｀)

---
+++ Assets/Scripts/effectsCollider/effectDestroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effectDestroy : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip smallBurst;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.PlayOneShot(smallBurst);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnParticleSystemStopped()
+    {
+        Destroy(this.gameObject);
+    }
+}
